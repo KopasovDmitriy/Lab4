48ce25843906d37c476c6bf19dcec8f181fb7fc7
KopasovDmitriy <Kopasov.dmitriy@gmail.com>
2018-06-07 20:20:44 +0800

    create python program

---
+++ program.py
@@ -0,0 +1,12 @@
+﻿import random
+
+class MyFor:
+    def ForN(n):
+        if(n<0):
+            return
+        print("Перебираем числа от 1 до " + str(n) + " :")
+        for i in range(0, n):
+            print("    i = " + str(i) + ", случайное число это " + str(random.randint(-i, i)))
+
+if __name__ == "__main__":
+    MyFor.ForN(int(input("Введите число (больше 1) n = ")))
